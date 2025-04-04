3da2f5e563ee65cddaa3516b892bfffd66aab4a6
ibrohim0610 <qoldoshevibrohim1@gmail.com>
2025-04-04 20:44:48 +0500

    just

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,9 +1,17 @@
 import 'package:go_router/go_router.dart';
 import 'package:store/core/routing/routes.dart';
 import 'package:store/features/onboarding/presentation/pages/onboarding_view.dart';
+import 'package:store/features/onboarding/presentation/pages/splash_screen.dart';
 
 final GoRouter router = GoRouter(
+  initialLocation: Routes.onboarding,
     routes: [
       GoRoute(path: Routes.onboarding,
-        builder: (context, state)=> OnboardingView(),),
-    ]);
+        builder: (context, state)=>
+            OnboardingView(),),
+      GoRoute(path: Routes.splashScreen,
+      builder: (context, state)=>
+      SplashScreen(),
+      ),
+    ]
+);
